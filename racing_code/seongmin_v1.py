ce0bb2d79bdd466da17ec7699e900fff62f1568e
boroboro01 <duke7201@naver.com>
2025-05-29 13:22:24 +0900

    feat: create seongmin-v1 model using upcoming turn angle

---
+++ racing_code/seongmin_v1.py
@@ -0,0 +1,50 @@
+import math
+
+def reward_function(params):
+    speed = params['speed']
+    waypoints = params['waypoints']
+    closest_idx = params['closest_waypoints'][1]
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    is_offtrack = params['is_offtrack']
+
+    if is_offtrack:
+        return 1e-3
+
+    reward = 1.0
+
+    # upcoming turn angle 계산
+    if closest_idx < len(waypoints) - 1:
+        next_point = waypoints[closest_idx]
+        prev_point = waypoints[closest_idx - 1]
+        next_next_point = waypoints[(closest_idx + 1) % len(waypoints)]
+
+        track_direction = math.degrees(math.atan2(
+            next_point[1] - prev_point[1],
+            next_point[0] - prev_point[0]
+        ))
+        upcoming_direction = math.degrees(math.atan2(
+            next_next_point[1] - next_point[1],
+            next_next_point[0] - next_point[0]
+        ))
+        turn_angle = abs(upcoming_direction - track_direction)
+        if turn_angle > 180:
+            turn_angle = 360 - turn_angle
+    else:
+        turn_angle = 0
+
+    # 직선 구간
+    if turn_angle < 10:
+        if distance_from_center >= 0.4 * track_width:
+            reward += 1.0  # 오른쪽 outline
+        if speed > 3.0:
+            reward += 1.0  # 빠른 속도
+
+    # 코너 구간
+    else:
+        if distance_from_center <= 0.1 * track_width:
+            reward += 1.0  # 왼쪽 인코스
+        if speed < 2.0:
+            reward += 1.0  # 감속
+
+    return float(reward)
